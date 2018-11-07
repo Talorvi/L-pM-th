5a37ec89c94e1514ba9f125c8b2d4803ee073477
Dawde <miesman@wp.pl>
2018-11-07 14:34:14 +0100

    Add files via upload

---
+++ eskuel.sql
@@ -0,0 +1,11 @@
+create database meta;
+
+using meta;
+
+create table Autorzy (id_autora int NOT NULL AUTO_INCREMENT, imie varchar(100));
+
+create table Kategorie (nazwa varchar(100));
+
+create table Wydawcy (nazwa varchar(100));
+
+create table Prace (id_pracy NOT NULL AUTO_INCREMENT, tytul varchar(255), jezyk varchar(100), rodzaj enum('artyku³','czasopismo'), rok_publikacji int, slowa_kluczowe varchar(100));
