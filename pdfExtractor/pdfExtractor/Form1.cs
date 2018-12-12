8c66953d621b7930b35cf48eee6f6a175a922f6d
Ja <Ja@DESKTOP-F7VJKI4>
2018-12-12 14:23:11 +0100

    Save changes

--- pdfExtractor/pdfExtractor/Form1.cs
+++ pdfExtractor/pdfExtractor/Form1.cs
@@ -21,9 +21,10 @@ namespace pdfExtractor
         }
 
         static string filePath;
-        static string Author = "", Title = "", Creator = "", Subject = "", Keywords = "", CreatedDate = "";
+        static string Author = "", Title = "", Creator = "", Language="",Type="", Subject = "", Keywords = "", CreatedDate = "";
+
         static PdfReader myreader;
-        WorkPiece workpiece = new WorkPiece(myreader, filePath, Author, Title, Creator, Subject, Keywords, CreatedDate); 
+        WorkPiece workpiece = new WorkPiece(myreader, filePath, Author, Title, Creator, Language, Type, Subject, Keywords, CreatedDate); 
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -32,6 +33,14 @@ namespace pdfExtractor
 
         private void ShowPDFbutton_Click(object sender, EventArgs e)
         {
+            AuthortextBox.Text = "";
+            TitletextBox.Text = "";
+            CreatortextBox.Text = "";
+            LanguagetextBox.Text = "";
+            TypetextBox.Text = "";
+            SubjecttextBox.Text = "";
+            KeywordstextBox.Text = "";
+            CreationDatetextBox.Text = "";
 
             OpenFileDialog dlg = new OpenFileDialog();
             
@@ -53,6 +62,8 @@ namespace pdfExtractor
                     AuthortextBox.Text = workpiece.Author;
                     TitletextBox.Text = workpiece.Title;
                     CreatortextBox.Text = workpiece.Creator;
+                    LanguagetextBox.Text = workpiece.Language;
+                    TypetextBox.Text = workpiece.Type;
                     SubjecttextBox.Text = workpiece.Subject;
                     KeywordstextBox.Text = workpiece.Keywords;
                     CreationDatetextBox.Text = workpiece.CreatedDate;
@@ -64,7 +75,16 @@ namespace pdfExtractor
                     {
                         MetadatalistBox.Items.Add(d.Key + ": " + d.Value);         
                     }
-                    reader.Close();                    
+                    reader.Close();
+                    PDFtextBox.Text = "";
+                    PDFtextBox.Text += "Author: " + workpiece.Author + Environment.NewLine;
+                    PDFtextBox.Text += "Title: " + workpiece.Title + Environment.NewLine;
+                    PDFtextBox.Text += "Creator: " + workpiece.Creator + Environment.NewLine;
+                    PDFtextBox.Text += "Language: " + workpiece.Language + Environment.NewLine;
+                    PDFtextBox.Text += "Type: " + workpiece.Type + Environment.NewLine;
+                    PDFtextBox.Text += "Subject: " + workpiece.Subject + Environment.NewLine;
+                    PDFtextBox.Text += "Keywords: " + workpiece.Keywords + Environment.NewLine;
+                    PDFtextBox.Text += "Creation Date: " + workpiece.CreatedDate + Environment.NewLine;
                 }
                 catch (Exception ex)
                 {
@@ -80,15 +100,29 @@ namespace pdfExtractor
             workpiece.Title = TitletextBox.Text;
             workpiece.Creator = CreatortextBox.Text;
             workpiece.Subject = SubjecttextBox.Text;
+            workpiece.Language = LanguagetextBox.Text;
+            workpiece.Type = TypetextBox.Text;
             workpiece.Keywords = KeywordstextBox.Text;
             workpiece.CreatedDate = CreationDatetextBox.Text;
 
-            workpiece.SetInfo(workpiece.FilePath, "Author", AuthortextBox.Text);
+            PDFtextBox.Text = "";
+            PDFtextBox.Text += "Author: " + workpiece.Author + Environment.NewLine;
+            PDFtextBox.Text += "Title: " + workpiece.Title + Environment.NewLine;
+            PDFtextBox.Text += "Creator: " + workpiece.Creator + Environment.NewLine;
+            PDFtextBox.Text += "Language: " + workpiece.Language + Environment.NewLine;
+            PDFtextBox.Text += "Type: " + workpiece.Type + Environment.NewLine;
+            PDFtextBox.Text += "Subject: " + workpiece.Subject + Environment.NewLine;
+            PDFtextBox.Text += "Keywords: " + workpiece.Keywords + Environment.NewLine;
+            PDFtextBox.Text += "Creation Date: " + workpiece.CreatedDate + Environment.NewLine;
+
+            /*workpiece.SetInfo(workpiece.FilePath, "Author", AuthortextBox.Text);
             workpiece.SetInfo(workpiece.FilePath, "Title", TitletextBox.Text);
             workpiece.SetInfo(workpiece.FilePath, "Creator", CreatortextBox.Text);
             workpiece.SetInfo(workpiece.FilePath, "Subject", SubjecttextBox.Text);
+            workpiece.SetInfo(workpiece.FilePath, "Language", LanguagetextBox.Text);
+            workpiece.SetInfo(workpiece.FilePath, "Type", TypetextBox.Text);
             workpiece.SetInfo(workpiece.FilePath, "Keywords", KeywordstextBox.Text);
-            workpiece.SetInfo(workpiece.FilePath, "CreatedDate", CreationDatetextBox.Text);
+            workpiece.SetInfo(workpiece.FilePath, "CreatedDate", CreationDatetextBox.Text);*/
         }
     }
 }
